7aa5ac61ea8aa2047f8fab7254b57b4a20dc6b6b
zym5368 <50917270+zym5368@users.noreply.github.com>
2025-05-07 17:47:34 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -16,7 +16,8 @@ sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/auto
 
 # Set etc/openwrt_release
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings3
-echo "DISTRIB_SOURCECODE='GZOS'" >>package/base-files/files/etc/openwrt_release
+sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='GZOS '" >> /etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
 sed -i 's/192.168.113.205/192.168.113.254/g' package/base-files/files/bin/config_generate
