9222c8a032c9ad01180bc0ee5d9f2c838f0641c6
zym5368 <50917270+zym5368@users.noreply.github.com>
2026-02-07 11:50:32 +0800

    Change default IP address in diy-part2.sh
    Updated default IP address from 192.168.110.115 to 192.168.113.206.

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -19,7 +19,7 @@ sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/auto
 sed -i "s|DISTRIB_DESCRIPTION='.*'|DISTRIB_DESCRIPTION='YMOS'|g" ${GITHUB_WORKSPACE}/openwrt/package/lean/default-settings/files/zzz-default-settings
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-sed -i 's/192.168.1.1/192.168.110.115/g' package/base-files/luci/bin/config_generate
+sed -i 's/192.168.1.1/192.168.113.206/g' package/base-files/luci/bin/config_generate
 sed -i "s/hostname='LEDE'/hostname='YMOS'/g" package/base-files/luci/bin/config_generate
 
 # Replace the default software source
